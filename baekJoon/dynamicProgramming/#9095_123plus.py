cc7f4f43896aa5077580b7c8507f58561e901c1d
Vigil2 <l2nak87@gmail.com>
2022-09-19 06:02:30 +0900

    2022 09 18 한글인식이안되서 일단 영어로 정리4

---
+++ baekJoon/dynamicProgramming/#9095_123plus.py
@@ -0,0 +1,24 @@
+
+
+
+""" 
+T = int(input())
+t = [0] * (n+1)
+
+print(d)
+
+for i in range(2, n+1):
+    d[i] = d[i-1] +1
+    if i%3 == 0:
+            d[i] = min(d[i], d[i // 3]+1)
+
+    if i%2 == 0:
+            d[i] = min(d[i], d[i // 2]+1)
+
+print(d[n]) """
+
+
+a= (1,2,3,4,5,6,7,8,9,10)
+b= a[4:7]
+print(b)
+print(a[:])
